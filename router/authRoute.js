848c5c77a09b021ad86a22e58d5a048df6c7db8e
farzanapomy <farzanapomy56@gmail.com>
2022-04-13 19:15:04 +0600

    viceo updated

--- router/authRoute.js
+++ router/authRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require("../models/userModel");
 const CryptoJS = require("crypto-js");
-
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
     const newUser = new User({
@@ -22,6 +22,7 @@ router.post('/register', async (req, res) => {
     }
 
 })
+
 // login
 
 router.post('/login', async (req, res) => {
@@ -31,21 +32,33 @@ router.post('/login', async (req, res) => {
         })
 
         !user && res.status(401).json("User Not Found !");
-
         const hashPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC).toString(CryptoJS.enc.Utf8);
-
-
-
-
         hashPassword !== req.body.password && res.status(401).json("Wrong password");
 
-        res.status(200).json(user);
+        const token = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SEC, { expiresIn: '7d' });
 
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, token });
     }
     catch (error) {
         res.status(500).json(error);
     }
 })
+
+
+router.delete('/logout', async (req, res) => {
+    const user = await User.findOne({
+        userName: req.body.userName
+    })
+    res.user("");
+    res.redirect('/home')
+
+})
+
 
 
 
