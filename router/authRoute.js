dd1fd4896d17521f3e98dfd85232bf352b511614
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2022-04-15 23:29:54 +0600

    url

--- router/authRoute.js
+++ router/authRoute.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) => {
     email: req.body.email,
     password: CryptoJS.AES.encrypt(
       req.body.password,
-      process.env.PASS_SEC
+      rentcarpro
     ).toString(),
   });
 
@@ -34,7 +34,7 @@ router.post("/login", async (req, res) => {
     !user && res.status(401).json("User Not Found !");
     const hashPassword = CryptoJS.AES.decrypt(
       user.password,
-      process.env.PASS_SEC
+      rentcarpro
     ).toString(CryptoJS.enc.Utf8);
     hashPassword !== req.body.password &&
       res.status(401).json("Wrong password");
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
         id: user._id,
         isAdmin: user.isAdmin,
       },
-      process.env.JWT_SEC,
+      rentcarpro,
       { expiresIn: "7d" }
     );
 
--- router/userRouter.js
+++ router/userRouter.js
@@ -7,7 +7,7 @@ router.put("/:id", verifyToken, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
-      process.env.PASS_SEC
+      rentcarpro
     ).toString();
   }
   try {
