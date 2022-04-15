ddc1041592891d06d63b2746f7cdf1c67eeb620a
farzanapomy <farzanapomy56@gmail.com>
2022-04-15 23:09:15 +0600

    remove .env file from git

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 module.export = app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const ConnectDB = require('./config/ConnectDB')
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
 
 // routes 
 const authRouter = require('./router/authRoute')
@@ -26,3 +27,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
+
