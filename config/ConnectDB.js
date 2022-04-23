2f60a07a407731a8ab274d94da005fbbef99d7f0
farzanapomy <farzanapomy56@gmail.com>
2022-04-22 19:40:40 -1200

    updated

--- config/ConnectDB.js
+++ config/ConnectDB.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 
 mongoose
-<<<<<<< HEAD
-  .connect(process.env.RENT_URL)
-=======
   .connect(`${process.env.RENT_URL}`)
->>>>>>> 7e5b5139bfff4bc4e389b08ecfc1977702eaf20f
   .then(() => {
     console.log("DB Connected");
   })
