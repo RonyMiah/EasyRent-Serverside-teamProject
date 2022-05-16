dc92aa245faacb1157893983fc5e3c5d6a90fa8c
RonyMiah <ronymiah2121@gmail.com>
2022-05-16 21:31:35 +0600

    deleted methode added done

--- router/getUserOrders.js
+++ router/getUserOrders.js
@@ -1,7 +1,6 @@
 const NewOrder = require("../models/createOrder");
-// const serviceModel = require("../models/Servicemodel");
 const router = require("express").Router();
-
+ 
 router.get("/orders/:email", async (req, res) => {
   try {
     const orders = await NewOrder.find({ userId: req.params.email }).populate({
@@ -13,18 +12,49 @@ router.get("/orders/:email", async (req, res) => {
     res.status(500).json({ err: err.massage });
   }
 });
-
+ 
 router.delete("/orders/delete/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const query = { _id: id };
-    const result = await NewOrder.findOneAndDelete(query);
-    res.json(result);
-  } 
-  
-  catch (err) {
+    const order = await NewOrder.findOneAndDelete(
+      { _id: id },
+      {
+        new: true,
+        upsert: true,
+      }
+    ).populate({
+      path: "serviceId",
+      model: "Service",
+    });
+    res.status(201).json(order);
+  } catch (error) {
     res.status(500).json({ err: err.massage });
   }
 });
-
+ 
+router.put("/orders/update/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const order = await NewOrder.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          ...data,
+        },
+      },
+      {
+        new: true,
+        upsert: true,
+      }
+    ).populate({
+      path: "serviceId",
+      model: "Service",
+    });
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({ err: err.massage });
+  }
+});
+ 
 module.exports = router;
