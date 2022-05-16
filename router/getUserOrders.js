f5dc436bbdbf90957dbaab3eeb09360fdf540971
RonyMiah <ronymiah2121@gmail.com>
2022-05-16 20:46:58 +0600

    done

--- router/getUserOrders.js
+++ router/getUserOrders.js
@@ -1,15 +1,30 @@
 const NewOrder = require("../models/createOrder");
+// const serviceModel = require("../models/Servicemodel");
 const router = require("express").Router();
 
-
 router.get("/orders/:email", async (req, res) => {
-    try{
-        const orders = await NewOrder.find({userId: req.params.email});
-        res.status(201).json(orders);
-    }  
-    catch(err){
-        res.status(500).json({err: err.massage})
-    }
+  try {
+    const orders = await NewOrder.find({ userId: req.params.email }).populate({
+      path: "serviceId",
+      model: "Service",
     });
+    res.status(201).json(orders);
+  } catch (err) {
+    res.status(500).json({ err: err.massage });
+  }
+});
 
-    module.exports = router;
+router.delete("/orders/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: id };
+    const result = await NewOrder.findOneAndDelete(query);
+    res.json(result);
+  } 
+  
+  catch (err) {
+    res.status(500).json({ err: err.massage });
+  }
+});
+
+module.exports = router;
