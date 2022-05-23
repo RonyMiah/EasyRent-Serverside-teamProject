60730b4432532ea16464cd03e813e596b87f60e5
strtanberahmed <tanber1111@gmail.com>
2022-05-24 00:53:24 +0300

    our drivers data  added

--- router/rentSinCarRouter.js
+++ router/rentSinCarRouter.js
@@ -116,21 +116,21 @@ router.get("/rentSingleOrder/:email", async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
-  router.delete('/rentCarsdelete/:id', (req, res) => {
 
-    rentSinCarModal.deleteOne({ _id: req.params.id }, (err) => {
-      if (err) {
-        res.status(500).send({
-          error: 'There was a server side error'
-        })
-      } else {
-        res.status(200).json({
-          message: "Order is deleted successfully"
-        })
-      }
-    }).clone()
+});
+router.delete('/rentCarsdelete/:id', (req, res) => {
 
+  rentSinCarModal.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.status(500).send({
+        error: 'There was a server side error'
+      })
+    } else {
+      res.status(200).json({
+        message: "Order is deleted successfully"
+      })
+    }
+  }).clone()
+});
 
-  });
-
-  module.exports = router;
+module.exports = router;
